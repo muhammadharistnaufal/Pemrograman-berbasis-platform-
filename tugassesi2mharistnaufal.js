531409b814eb9d5e204c0c72b60119d47765b566
muhammadharistnaufal <114551807+muhammadharistnaufal@users.noreply.github.com>
2023-10-03 06:27:10 +0700

    Add files via upload

---
+++ tugassesi2mharistnaufal.js
@@ -0,0 +1,11 @@
+// Deklarasi variabel firstName dengan nilai nama depan Anda
+var firstName = "NamaDepan";
+
+// Deklarasi variabel lastName dengan nilai nama belakang Anda
+var lastName = "NamaBelakang";
+
+// Deklarasi variabel age dengan nilai umur Anda
+var age = 25; // Gantilah dengan umur Anda
+
+// Deklarasi variabel isMarried dengan nilai boolean sesuai keadaan Anda
+var isMarried = false; // Gantilah dengan true jika Anda sudah menikah, false jika belum
