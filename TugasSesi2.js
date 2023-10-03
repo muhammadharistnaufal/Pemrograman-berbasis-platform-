70a759ca9a90ef630cb8ad99c7b89363f6df163a
muhammadharistnaufal <114551807+muhammadharistnaufal@users.noreply.github.com>
2023-10-03 13:49:27 +0700

    Add files via upload

---
+++ TugasSesi2.js
@@ -0,0 +1,10 @@
+const firstName = "Muhammad Harist Naufal";  // tipe data string
+const lastName = "Harist";  // tipe data string
+const age = 19;  // tipe data number
+const isMarried = false;  // tipe data boolean
+
+// Menampilkan nilai variabel
+console.log("Nama Depan:", firstName);
+console.log("Nama Belakang:", lastName);
+console.log("Umur:", age);
+console.log("Status Pernikahan:", isMarried);
